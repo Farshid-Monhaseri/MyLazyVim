23c05ea8e829c1af0c0ff714fa7ea6f7cf6ed4e2
Farshid Monhaseri <monhaseri.f@gmail.com>
2023-11-17 18:04:40 +0100

    Add couple of langauge support
    - Added C++, Rust, Golang, Java and someother langauge clients.
    
    Signed-off-by: Farshid Monhaseri <monhaseri.f@gmail.com>

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,10 +12,12 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    --{ import = "lazyvim.plugins.extras.lang.cpp" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
