2cfecf4c4e3506e2872d166bb02bda30e1c1e460
Farshid Monhaseri <farshid.monhaseri@boule.com>
2024-04-21 00:18:39 +0200

    Add Rust config

---
+++ lua/plugins/rust.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "codelldb" })
+    end,
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    opts = function(_, opts)
+      opts.adapters = opts.adapters or {}
+      vim.list_extend(opts.adapters, {
+        require("rustaceanvim.neotest"),
+      })
+    end,
+  },
+}
